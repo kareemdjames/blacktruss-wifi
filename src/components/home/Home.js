1eacd6acdbe250b4030cbcb7cf870e23878747f7
Kareem James <kareemdjames@gmail.com>
2018-09-01 19:27:27 -0400

    creatd home component and css

---
+++ src/components/home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+import "./Home.css";
+
+class Home extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello, From Home Component</h1>
+      </Fragment>
+    );
+  }
+}
+
+export default Home;
