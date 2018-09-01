493a22d4513c0adaddf41c101cf251b5b7610c92
Kareem James <kareemdjames@gmail.com>
2018-09-01 19:27:16 -0400

    added home component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
+import Home from "../home/Home";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello Kareem, from React</h1>
+        <Home />
       </div>
     );
   }
