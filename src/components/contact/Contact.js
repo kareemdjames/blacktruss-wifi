6af4e7bccb0008ca2b86c5bd25060f1b81cee100
Kareem James <kareemdjames@gmail.com>
2018-09-03 23:24:38 -0400

    created initial contact component

---
+++ src/components/contact/Contact.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from "react";
+
+class Contact extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello, from Contact component</h1>
+      </Fragment>
+    );
+  }
+}
+
+export default Contact;
