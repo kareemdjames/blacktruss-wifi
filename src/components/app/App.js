d67ffbc28b09654866240b70d04a203e54dcc988
Kareem James <kareemdjames@gmail.com>
2018-09-03 23:24:16 -0400

    added contact component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Home from "../home/Home";
+import Contact from "../contact/Contact";
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="App">
         <h1>Hello Kareem, from React</h1>
         <Home />
+        <Contact />
       </div>
     );
   }
